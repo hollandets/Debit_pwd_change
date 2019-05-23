5a8b34b640c5719b34de8bec9dd9b7dd9f40b07d
Roman Bondarenko <roman.bondarenko@idt.net>
2019-05-23 11:56:46 +0300

    Rewrite logic for Solaris SunOS sed and crypt commands

--- update_pass.sh
+++ update_pass.sh
@@ -14,16 +14,26 @@ encrypt_key=/u/debit/lib
 for script_name in ${file_list}
 do
   if [ -f "$script_name" ]; then
-    echo "Start with File:       ${script_name}"
-    echo "Copying script to backup file ..."
+    echo "Start with File:    ${script_name}"
+    echo "    Copying script to backup file ..."
     cp ${script_name} ${script_name}_backup_${now}
-    echo "Changing password in script"
+    echo "    Changing password in script ..."
 #decrypt old file and put its content to temporary file
     crypt $encrypt_key < ${script_name}_backup_${now} > ./temporary_file
 #old_pwd - is an old password, change it to the new one
-    sed -i "s/${old_pwd}/${new_pwd}/g" ./temporary_file
-#encrypt new password
-    cat ./temporary_file | crypt --encrypt $encrypt_key > ${script_name}
+    if [ $(uname) == "SunOS" ]; then
+#changing all the entries of old_pwd to new_pwd via creating temp file for Solaris SunOS
+      sed "s/${old_pwd}/${new_pwd}/g" ./temporary_file > ./temp_solaris.tmp && mv ./temp_solaris.tmp ./temporary_file
+#encrypt new password for Solaris SunOS
+      cat ./temporary_file | crypt $encrypt_key > ${script_name}
+      rm ./temp_solaris.tmp
+    else
+#changing all the entries of old_pwd to new_pwd in decrypted temporary file
+      sed -i "s/${old_pwd}/${new_pwd}/g" ./temporary_file
+#encrypt new password file for Linux
+      cat ./temporary_file | crypt --encrypt $encrypt_key > ${script_name}
+    fi
     rm ./temporary_file
+    echo "Finished with File:  ${script_name}"
   fi
 done
